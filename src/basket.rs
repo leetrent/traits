c80e1fa459160d1496b5cab8eda1d719f76736ef
Lee Trent <lee.trent.1@gmail.com>
2024-11-11 20:01:27 -0600

    Got basket of strings workings

---
+++ src/basket.rs
@@ -0,0 +1,23 @@
+pub struct Basket {
+    pub item: Option<String>,
+}
+
+impl Basket {
+    pub fn new(item: String) -> Self {
+        Basket {
+            item: Some(item)
+        }
+    }
+
+    pub fn get (&mut self) -> Option<String> {
+        self.item.take()
+    }
+
+    pub fn put(&mut self, item: String) {
+        self.item = Some(item);
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.item.is_none()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod basket;
+use basket::Basket;
+
 fn main() {
-    println!("Hello, world!");
+    println!();
+    let mut b1 = Basket::new(String::from("Casey"));
+    println!("{:?}", b1.get());
+
+    b1.put(String::from("Casey Bo Trent"));
+    println!("{:?}", b1.get());
+
+    println!("{:?}", b1.is_empty());
+    println!();
 }
