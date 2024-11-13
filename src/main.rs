4d72a9637753cb23a5599cf042b1e10010b9925a
Lee Trent <lee.trent.1@gmail.com>
2024-11-12 21:27:29 -0600

    Done

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,31 @@
 mod basket;
 mod stack;
+mod container;
 
 use basket::Basket;
 use stack::Stack;
+use container::Container;
 
 fn main() {
     do_basket();
     do_stack();
 }
 
+fn add_string<T: Container<String>>(c: &mut T, s: String) {
+    c.put(s);
+}
+
 fn do_basket() {
     println!();
     let mut b1 = Basket::new(String::from("Casey"));
-    println!("{:?}", b1.get());
+    // let b2 = Basket::new(17);
+    // let b3 = Basket::new(false);
 
-    b1.put(String::from("Casey Bo Trent"));
-    println!("{:?}", b1.get());
-
-    println!("{:?}", b1.is_empty());
-    println!();
-
-    println!();
-    let mut b2 = Basket::new(17);
-    println!("{:?}", b2.get());
-
-    println!();
-    let mut b3 = Basket::new(false);
-    println!("{:?}", b3.get());
+    add_string(&mut b1, String::from(" Bo Trent"));
 }
 
 fn do_stack() {
-    let s1 = Stack::new(vec![String::from("Casey Bo Trent")]);
+    let mut s1 = Stack::new(vec![String::from("Casey")]);
+    // let s2 = Stack::new(vec![1,2,3]);
+    add_string(&mut s1, String::from(" Bo Trent"));
 }
--- src/stack.rs
+++ src/stack.rs
@@ -1,21 +1,24 @@
-pub struct Stack {
-    items: Vec<String>
+use super::container::Container;
+
+pub struct Stack<T> {
+    items: Vec<T>
 }
 
-impl Stack {
-    pub fn new(items: Vec<String>) -> Self {
+impl <T>Stack<T> {
+    pub fn new(items: Vec<T>) -> Self {
         Stack { items }
     }
-
-    pub fn get(&mut self) -> Option<String> {
+}
+impl <T> Container<T> for Stack<T> {
+    fn get(&mut self) -> Option<T> {
         self.items.pop()
     }
 
-    pub fn put(&mut self, item: String) {
+    fn put(&mut self, item: T) {
         self.items.push(item);
     }
     
-    pub fn is_empty(&self) -> bool {
+    fn is_empty(&self) -> bool {
         self.items.is_empty()
     }   
 }
